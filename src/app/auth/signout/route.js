1a2c0f0a230da921b446bd257e7cc38e2f3f8709
maulanasigit20 <sigitmaulana20@gmail.com>
2026-06-13 19:06:08 +0700

    fix logout redirect on vercel

--- src/app/auth/signout/route.js
+++ src/app/auth/signout/route.js
@@ -1,10 +1,12 @@
 import { createClient } from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(request) {
   const supabase = await createClient();
 
   await supabase.auth.signOut();
 
-  return NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_SITE_URL));
+  return NextResponse.redirect(
+    new URL("/login", request.url)
+  );
 }
